25983a1a5211bb8077605c3f3a461a26ba98935e
Sudhir Gupta <u6458881@anu.edu.au>
2019-09-08 15:45:41 +1000

    Flask model updated.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,render_template
 from Get_Distance import *
+from Model.Inference import *
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,7 +14,13 @@ def my_form_post():
     lat,lng=map(float,processed_text.split(" "))
     busstop_data_path = './Data/Bus_Stops.csv'
     busstop_df = pd.read_csv(busstop_data_path)
-    return render_template("OutputPage.html",distance=get_distance_next_stop(lat,lng,busstop_df))
+    build_or_not=""
+    if test_predict[0]==-1:
+        build_or_not="cannot build"
+    else:
+        build_or_not = "can build"
+    print(list(test_data.iloc[0,:].values))
+    return render_template("OutputPage.html",test_data=list(test_data.iloc[0,:].values),build_or_not=build_or_not)
 
 @app.route("/details/")
 def details():
